b6c9527295f51d3b0e8204c56554b8b91f335b7d
Zachary Rojas <ztr2194@gmail.com>
2023-03-09 12:05:10 -0600

    dom nodes lab done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const element = document.querySelector('main');
+element.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "Zachary is the champion";
+
